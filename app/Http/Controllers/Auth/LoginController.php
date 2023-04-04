2933d66be9a28f05f16f9d9a89d153e8f94926a2
Chumichyov <chumichyooov@gmail.com>
2023-04-04 23:40:13 +0300

    delete dd in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,7 +12,6 @@ class LoginController extends Controller
     public function __invoke(LoginRequest $request)
     {
         $credentials  = $request->validated();
-        dd($credentials);
 
         if (!auth()->attempt($credentials)) {
             throw new GeneralJsonException('Неправильный логин и/или пароль', 422);
